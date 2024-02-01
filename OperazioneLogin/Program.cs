95df6354b3952e480ae2c4d8e04946f5c176245c
SabinoOrbello <gemellus95@outlook.it>
2024-02-01 16:27:14 +0100

    Aggiungere i file di progetto.

---
+++ OperazioneLogin/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperazioneLogin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int scelta;
+            do
+            {
+                // menù operazioni
+
+                Console.WriteLine("===============OPERAZIONI==============");
+                Console.WriteLine("Scegli l'operazione da effettuare:");
+                Console.WriteLine("1.: Login");
+                Console.WriteLine("2.: Logout");
+                Console.WriteLine("3.: Verifica ora e data di login");
+                Console.WriteLine("4.: Lista degli accessi");
+                Console.WriteLine("5.: Esci");
+                Console.WriteLine("========================================");
+
+                // Legge l'input dell'utente e gestisce la scelta
+
+                if (int.TryParse(Console.ReadLine(), out scelta))
+                {
+                    switch (scelta)
+                    {
+                        case 1:
+                            Utente.Login();
+                            break;
+                        case 2:
+                            Utente.Logout();
+                            break;
+                        case 3:
+                            Utente.VerificaOraDataLogin();
+                            break;
+                        case 4:
+                            Utente.ListaAccessi();
+                            break;
+                        case 5:
+                            Console.WriteLine("Arrivederci trimone");
+                            break;
+                        default:
+                            Console.WriteLine("Scelta non valida. Riprova");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Inserisci un numero valido");
+                }
+            } while (scelta != 5); // ripeti finchè l'utente non sceglie di uscire
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OperazioneLogin/Utente.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperazioneLogin
+{
+    // Classe statica Utente che gestisce l'autenticazione e l'accesso
+    public static class Utente
+    {
+        private static string username;
+        private static string password;
+        private static DateTime? dataLogin; // Data e ora dell'ultimo login
+        private static List<DateTime> storicoAccessi = new List<DateTime>(); // storico degli accessi
+
+
+        // metodo per effettuare il login
+        public static void Login()
+        {
+            Console.Write("Inserisci username: ");
+            string inputUsername = Console.ReadLine();
+
+            Console.Write("Inserisci password: ");
+            string inputPassword = Console.ReadLine();
+
+            Console.Write("Conferma password: ");
+            string confermaPassword = Console.ReadLine();
+
+            // Controlla se l'input è valido ed effettua il login
+            if(!string.IsNullOrEmpty(inputUsername) && inputPassword == confermaPassword)
+            {
+                username = inputPassword;
+                password = inputPassword;
+                dataLogin = DateTime.Now;
+                storicoAccessi.Add(dataLogin.Value);
+
+                Console.WriteLine("Login effettuato con successo!");
+            }
+            else
+            {
+                Console.WriteLine("Errore durante il login. Assicurati di aver inserito correttamente username e password");
+            }
+                
+        }
+
+        // metodo per effettuare il logout
+        public static void Logout()
+        {
+            if(IsUtenteAutenticato())
+            {
+                username = null;
+                password = null;
+                dataLogin = null;
+
+                Console.WriteLine("Logout effettuato con successo");
+            }
+            else
+            {
+                Console.WriteLine("Messun utente autenticato. Impossibile effettuare il logout");
+            }
+        }
+
+        // metodo per verificare l'ora e la data dell'ultimo login
+        public static void VerificaOraDataLogin()
+        {
+            if(IsUtenteAutenticato())
+            {
+                Console.WriteLine($"Ultimo accesso effettuato il: {dataLogin}");
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente autenticato. Impossibile verificare l'ora e la data di login");
+            }
+        }
+
+
+        // metodo per mostrare la lista degli accessi
+        public  static void ListaAccessi()
+        {
+            if(storicoAccessi.Count > 0)
+            {
+                Console.WriteLine("Storico accessi: ");
+                foreach (var accesso in storicoAccessi)
+                {
+                    Console.WriteLine(accesso);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nessun accesso registrato");
+            }
+        }
+
+
+        // metodo privato per verificare se un utente è autenticato
+        public static bool IsUtenteAutenticato()
+        {
+            return !string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password);
+        }
+    }
+}
